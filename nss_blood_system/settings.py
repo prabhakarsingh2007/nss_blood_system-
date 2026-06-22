1f707f78971a41db6a3ad1224b55ed07e062b3d4
prabhakarsingh2007 <prabhakarkumarsingh578@gmail.com>
2026-06-22 11:31:33 +0530

    fix: configure STATIC_ROOT and Whitenoise storage for deployment

--- nss_blood_system/settings.py
+++ nss_blood_system/settings.py
@@ -91,8 +91,15 @@ TIME_ZONE = "Asia/Kolkata"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
